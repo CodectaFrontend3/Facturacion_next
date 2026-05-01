4cf8fef3c44f5dff51add6e8b565b495fbcbb3c5
crodrigoDev <212727257+crodrigoDev@users.noreply.github.com>
2026-05-01 12:58:55 -0500

    feat: Header - Se creo el diseño del header asi como sus interface y data de prueba

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
-import AppSidebar from "@/components/app-sidebar";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import AppSidebar from "@/components/layout/app-sidebar";
+import AppHeader from "@/components/layout/app-header";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -25,7 +27,10 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col font-sans">
         <AppSidebar>
-          <main className="flex min-w-0 flex-1 flex-col">{children}</main>
+          <main className="flex min-w-0 flex-1 flex-col">
+            <AppHeader />
+            <div className="flex-1">{children}</div>
+          </main>
         </AppSidebar>
       </body>
     </html>
